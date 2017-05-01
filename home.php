9461ec272061650b86bd92eb6be2708006334b21
Tyler Wang <twang.123@live.com>
2017-05-01 15:13:42 -0400

    check home page

--- home.php
+++ home.php
@@ -1,4 +1,13 @@
-<!DOCTYPE HTML>
+<?php session_start(); $page = 'home';?>
+
+<!DOCTYPE html>
 <html>
+<head>
+  <title>HOME</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <?php include 'navbar.php';?>
+</body>
 
 </html>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,16 @@
-<!DOCTYPE HTML>
-<html>
+<h1>seniorcitizenconnect</h1>
+<ul>
+  <li><a <?php echo ($page == 'home') ? 'class="active"' : '';?> href="home.php">Home</a></li>
+  <li><a <?php echo ($page == 'forum') ? 'class="active"' : '';?> href="forum.php">Forum</a></li>
+  <li><a <?php echo ($page == 'requests') ? 'class="active"' : '';?> href="requests.php">Requests</a></li>
+  <li><a <?php echo ($page == 'offers') ? 'class="active"' : '';?> href="offers.php">Offers</a></li>
+  <li><input type="text" name="search" placeholder="Search...">
+  <?php
+  if ( isset( $_SESSION[ 'logged_user' ] ) ) {
+    $username = $_SESSION[ 'logged_user' ];
+    print ("<li class='user'>Hello, $username</li>");
+  }
 
-</html>
+  ?>
+
+</ul>
