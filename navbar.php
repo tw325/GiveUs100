ffcc8818a9ec3223900acf46d87a19c4d9edc33a
Ryan Feldman <rpf53@cornell.edu>
2017-05-01 17:14:18 -0400

    Started navbar css - will finish in a bit if nobody else does

--- navbar.php
+++ navbar.php
@@ -1,16 +1,18 @@
 <h1>seniorcitizenconnect</h1>
-<ul>
-  <li><a <?php echo ($page == 'home') ? 'class="active"' : '';?> href="home.php">Home</a></li>
-  <li><a <?php echo ($page == 'forum') ? 'class="active"' : '';?> href="forum.php">Forum</a></li>
-  <li><a <?php echo ($page == 'requests') ? 'class="active"' : '';?> href="requests.php">Requests</a></li>
-  <li><a <?php echo ($page == 'offers') ? 'class="active"' : '';?> href="offers.php">Offers</a></li>
-  <li><input type="text" name="search" placeholder="Search...">
-  <?php
-  if ( isset( $_SESSION[ 'logged_user' ] ) ) {
-    $username = $_SESSION[ 'logged_user' ];
-    print ("<li class='user'>Hello, $username</li>");
-  }
+<div id="menubar">
+	<ul id="menu">
+		<li><a <?php echo ($page == 'home') ? 'class="active"' : '';?> href="home.php">Home</a></li>
+		<li><a <?php echo ($page == 'forum') ? 'class="active"' : '';?> href="forum.php">Forum</a></li>
+		<li><a <?php echo ($page == 'requests') ? 'class="active"' : '';?> href="requests.php">Requests</a></li>
+		<li><a <?php echo ($page == 'offers') ? 'class="active"' : '';?> href="offers.php">Offers</a></li>
+		<li><input type="text" name="search" placeholder="Search...">
+		<?php
+		if ( isset( $_SESSION[ 'logged_user' ] ) ) {
+			$username = $_SESSION[ 'logged_user' ];
+			print ("<li class='user'>Hello, $username</li>");
+		}
 
-  ?>
+		?>
 
-</ul>
+	</ul>
+</div>
