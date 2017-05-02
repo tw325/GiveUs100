5975cfbc753c0dae3d08f5a3ac4bd0a2a5029e6b
Ryan Feldman <rpf53@cornell.edu>
2017-05-02 00:26:31 -0400

    Navbar back

--- profile.php
+++ profile.php
@@ -1,4 +1,16 @@
-<!DOCTYPE HTML>
+<?php session_start(); $page = 'profile';?>
+
+<!DOCTYPE html>
 <html>
+<head>
+  <title>HOME</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <?php include 'navbar.php';?>
+  <div id="profile">
+  	<img src= img/tempProfile.jpg onerror = "this.src=\'http://i.imgur.com/hPYOVf9.jpg\';" alt="pic"/>
+  </div>
+</body>
 
 </html>
