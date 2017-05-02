b39fc7c5a983a5139fffe454d7afb4c679bb1190
Tyler Wang <twang.123@live.com>
2017-05-02 16:27:24 -0400

    changed config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
     // database host//
     define('DB_HOST', 'localhost' );
     //database name - info230_SP17_username//
-    define('DB_NAME', '' );  
+    define('DB_NAME', 'giveus100' );
     //Course Server username//
-    define('DB_USER', '');  
-    //password// 
-    define('DB_PASSWORD', ''); 
-?> 
+    define('DB_USER', 'fp_giveus100');
+    //password//
+    define('DB_PASSWORD', 'Ysc!v4a24G+tsN9X');
+?>
