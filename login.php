97d6a662d0b4277172e5b4e2ef9a22b891b76256
ut33 <ut33@cornell.edu>
2017-05-16 15:09:47 -0400

    Update login.php

--- login.php
+++ login.php
@@ -10,51 +10,50 @@
   <?php include 'navbar.php';?>
 
   <?php
-  $username = filter_input( INPUT_POST, 'username', FILTER_SANITIZE_STRING );
-  $password = filter_input( INPUT_POST, 'password', FILTER_SANITIZE_STRING );
-  if ( isset($_SESSION['logged_user']) ) {
-    $username = $_SESSION['logged_user'];
-    print ("<h1> Welcome back, $username</h1>");
+      $username = filter_input( INPUT_POST, 'username', FILTER_SANITIZE_STRING );
+      $password = filter_input( INPUT_POST, 'password', FILTER_SANITIZE_STRING );
+      if ( isset($_SESSION['logged_user']) ) {
+        $username = $_SESSION['logged_user'];
+        print ("<h1> Welcome back, $username</h1>");
 
-  } else if ( empty( $username ) || empty( $password ) ){
-    ?>
-    <h1> Log In </h1>
-    <table>
-      <form action="login.php" method="post">
-        <tr><td><p>Username:</p><input type="text" name="username"></td></tr>
-        <tr><td><p>Password:</p><input type="password" name="password"></td></tr>
-        <tr><td><input type="submit" value="Submit"></td></tr>
-      </form>
-      <form method="post" action="createAccount.php">
-        <tr><td><input type="submit" name="submit" value="Create an Account"></td></tr>
-      </form>
-    </table>
-
-    <?php
-  } else{
-    require_once 'config.php';
-    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME );
-    $query = "SELECT * FROM users WHERE username = '$username'";
-    $result = $mysqli->query($query);
-    if ($result && $result->num_rows == 1) {
-      $row = $result->fetch_assoc();
-      $db_hash_password = $row[ 'password' ];
-      if( password_verify($password, $db_hash_password) ) {
-        $_SESSION['logged_user'] = $username;
-        header( "refresh:0;url=index.php" );
+      } else if ( empty( $username ) || empty( $password ) ){
+        ?>
+        <h1> Log In </h1>
+        <form action="login.php" method="post">
+          <table>
+              <tr><td>Username:</td> <td><input type="text" name="username"></td>
+              <tr><td>Password: </td> <td><input type="password" name="password"></td>
+          </table>
+          <br><input type="submit" value="Submit">
+        </form>
+        <form method="post" action="createAccount.php">
+	      <input type="submit" name="submit" value="Create an Account">
+	      </form>
+        <?php
+      } else{
+        require_once 'config.php';
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME );
+        $query = "SELECT * FROM users WHERE username = '$username'";
+        $result = $mysqli->query($query);
+        if ($result && $result->num_rows == 1) {
+          $row = $result->fetch_assoc();
+          $db_hash_password = $row[ 'password' ];
+          if( password_verify($password, $db_hash_password) ) {
+            $_SESSION['logged_user'] = $username;
+            header( "refresh:0;url=index.php" );
+          }
+          else {
+            echo '<h1>You did not login successfully.</h1>';
+            echo '<h2>Please <a href="login.php">login</a></h2>';
+          }
+        }
+        else {
+          echo '<h1>You did not login successfully.</h1>';
+          echo '<h2>Please <a href="login.php">login</a></h2>';
+        }
       }
-      else {
-        echo '<h1>You did not login successfully.</h1>';
-        echo '<h2>Please <a href="login.php">login</a></h2>';
-      }
-    }
-    else {
-      echo '<h1>You did not login successfully.</h1>';
-      echo '<h2>Please <a href="login.php">login</a></h2>';
-    }
-  }
 
-  ?>
+       ?>
   <?php include 'footer.php';?>
 </body>
 
