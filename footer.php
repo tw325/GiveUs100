8d8c0559ce518c5beba8a518e457a171ea548480
Tyler Wang <twang.123@live.com>
2017-05-16 15:34:47 -0400

    removed help

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
 <div>
 <ul id="footer">
   <li><a <?php echo ($page == 'contact') ? 'class="active"' : '';?> href="contact.php">Contact Us</a></li>
-  <li><a <?php echo ($page == 'help') ? 'class="active"' : '';?> href="help.php">Help</a></li>
   <li><a <?php echo ($page == 'about') ? 'class="active"' : '';?> href="about.php">About Us</a></li>
   <li style="float:right"> 2017 Senior Citizen Connect</li>
 </ul>
